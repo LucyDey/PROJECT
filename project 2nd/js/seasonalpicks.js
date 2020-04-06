aff347f7d6198361466a4686ac30b11f852e6d30
e-mu4 <61795820+e-mu4@users.noreply.github.com>
2020-04-06 12:10:14 +0100

    Update seasonalpicks.js

--- project 2nd/js/seasonalpicks.js
+++ project 2nd/js/seasonalpicks.js
@@ -1,24 +1,30 @@
-var image = ['/rules', '/100hours', '/platillo-voladores', '/chenmapo', '/balthazar'];
+var slideIndex = 1;
+showSlides(slideIndex);
 
-var i = image.length;
-//function for next slide
-function nextImage(){
-  if (i>image.length) {
-    i= i+1;
-      }else{
-        i = 1;
-      }
-      slider_content.innerHTML = `<img src="${image[i-1]}.jpg">`;
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function prewImage(){
-  if (i<image.length) {
-  i= i-1;
-    }else{
-      i = image.length ;
-    }
-    slider_content.innerHTML = `<img src="${image[i-1]}.jpg">`;
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
 
 // script for auto slider
