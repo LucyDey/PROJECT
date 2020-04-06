fd4f539407a8aa01266d500102f654fc8da318ca
e-mu4 <61795820+e-mu4@users.noreply.github.com>
2020-04-06 13:16:08 +0100

    Update seasonalpicks.js

--- project 2nd/js/seasonalpicks.js
+++ project 2nd/js/seasonalpicks.js
@@ -1,3 +1,8 @@
+var slider_content
+document.addEventListener("DOMContentLoaded", function(){
+   slider_content =  document.getElementById('box');
+})
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -25,6 +30,30 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}
+
+// script for auto slider
+setInterval(nextImage , 6000)var image = ['/rules', '/balthazar', '/chenmapo', '/platillo-voladores', '/100hours'];
+
+var i = image.length;
+//function for next slide
+function nextImage(){
+  if (i>image.length) {
+    i= i+1;
+      }else{
+        i = 1;
+      }
+      slider_content.innerHTML = `<img src="${image[i-1]}.jpg">`;
+}
+
+function prewImage(){
+  if (i<image.length) {
+  i= i-1;
+    }else{
+      i = image.length ;
+    }
+    slider_content.innerHTML = `<img src="${image[i-1]}.jpg">`;
+
 }
 
 // script for auto slider
