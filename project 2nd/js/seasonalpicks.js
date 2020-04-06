30baecfd017743bd5240e47cf9e5364aa145e850
e-mu4 <61795820+e-mu4@users.noreply.github.com>
2020-04-06 16:18:23 +0100

    Update seasonalpicks.js

--- project 2nd/js/seasonalpicks.js
+++ project 2nd/js/seasonalpicks.js
@@ -1,60 +1,35 @@
-var slider_content
-document.addEventListener("DOMContentLoaded", function(){
-   slider_content =  document.getElementById('box');
-})
+var totalSlides=document.querySelector('.slider-items').children;
+var nextSlide=document.querySelector(".right-slide");
+var prevSlide=document.querySelector(".left-slide")
+var totalSlides=slides.length;
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+nextSlide.onclick=function(){
+   next("next");
 }
+prevSlide.onclick=function(){
+   prev("prev");
+}   
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
-// script for auto slider
-setInterval(nextImage , 6000)var image = ['rules', 'balthazar', 'chenmapo', 'platillo-voladores', '100hours'];
-
-var i = image.length;
-//function for next slide
-function nextImage(){
-  if (i>image.length) {
-    i= i+1;
-      }else{
-        i = 1;
+function next(direction){
+     
+   if(direction=="next"){
+      index++;
+      if(index==totalSlides){
+         index=0;
       }
-      slider_content.innerHTML = `<img src="${image[i-1]}.jpg">`;
+   }
+   else{
+      if(index==0){
+         index=totalSlides-1;
+      }
+      else{
+         index--;
+   }       
+   
+   for(i=0;i<slides.length;i++){
+     slides[i].classList.remove("active");
 }
-
-function prewImage(){
-  if (i<image.length) {
-  i= i-1;
-    }else{
-      i = image.length ;
-    }
-    slider_content.innerHTML = `<img src="${image[i-1]}.jpg">`;
-
+     slides[index].classList.add("active");
+   
 }
-
-// script for auto slider
-setInterval(nextImage , 6000)
